2a99b4f59a6b4395bc9c1b2b1c400fa2d88085e4
cnmade <hufeng1987@gmail.com>
2022-06-15 09:55:24 +0800

    启用自动聚焦键盘开启

--- lib/screens/booking/booking_home.dart
+++ lib/screens/booking/booking_home.dart
@@ -60,6 +60,7 @@ class _BookingHomeState extends State<BookingHome> {
               children: [
                 Text('金额:'),
                 TextFormField(
+                  autofocus: true,
                   keyboardType: const TextInputType.numberWithOptions(signed:false, decimal: true),
                   // The validator receives the text that the user has entered.
                   validator: (value) {
